5d58431f5fe8737cd3210640cc48a2a0e6196379
Martinez <martinez@raidho.mx>
2015-10-08 22:21:54 -0500

    Post action added to routes/users.js to get Form Values

--- routes/users.js
+++ routes/users.js
@@ -6,18 +6,25 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET users listing. */
 router.get('/register', function(req, res, next) {
   res.render('register', {
 	  'title': 'Register'
   });
 });
 
-/* GET users listing. */
 router.get('/login', function(req, res, next) {
   res.render('login', {
 	  'title': 'Login'
   });
+});
+
+router.post('/register',function(req, res, next){
+	// Get Form Values
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
 });
 
 module.exports = router;
