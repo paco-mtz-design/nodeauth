65b956dae408585c972273ad339d89abb231e36a
Martinez <martinez@raidho.mx>
2015-10-08 21:51:10 -0500

    Register route added to route/users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/register', function(req, res, next) {
+  res.render('register', {
+	  'title': 'Register'
+  });
+});
+
 module.exports = router;
